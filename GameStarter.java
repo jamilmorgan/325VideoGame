67823a919f648bbbb0c52bd7e3e614ff45f1e07e
Juhmiil <104172788+Juhmiil@users.noreply.github.com>
2022-04-25 16:04:17 -0400

    Update GameStarter.java

--- GameStarter.java
+++ GameStarter.java
@@ -18,17 +18,49 @@ public class GameStarter extends JFrame implements ActionListener {
 
         pnlContain.add(btnAbout);
         pnlContain.add(btnStart);
+
+        cp.add(pnlContain);
     }
 
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource() == btnStart) {
-            
+           JTextArea GameSession;
+
+            JFrame jf = new JFrame();
+            JPanel jp = new JPanel();
+            JButton jb = new JButton("Just click it");
+            jb.addActionListener(this);
+            GameSession = new JTextArea(10,20);
+            GameSession.setLineWrap(true);
+            JScrollPane scroller = new JScrollPane(GameSession);
+            scroller.setVerticalScrollBarPolicy(
+                    ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+            scroller.setHorizontalScrollBarPolicy(
+                    ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+            jp.add(scroller);
+            jf.getContentPane().add(BorderLayout.CENTER,jp);
+            jf.getContentPane().add(BorderLayout.SOUTH,jb);
+
+
+            jf.setSize(500,500);
+            jf.setVisible(true);
 
         }
         if (evt.getSource() == btnAbout) {
 
 
         }
+
+
+    }
+
+    public static void main(String[] args) {
+        GameStarter my = new GameStarter();
+        my.setSize(500,500);
+        my.setVisible(true);
+
     }
 
 }
+
+
